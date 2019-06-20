79c741f94ebbc86c64c0e66c40e40d12865dea0d
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-06-21 07:50:42 +0800

    Agregando entidad Personas al contexto

---
+++ Tarea6/DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarea6.Entidades;
+
+namespace Tarea6.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Personas> Personas { get; set; }
+        public Contexto() : base("Constr")
+        {
+
+        }
+    }
+}
