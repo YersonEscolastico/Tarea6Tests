27aaa677fa58c553d2a0def4e9689b39390a89f4
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-06-21 13:29:57 +0800

    Haciendo Tests de Usuarios

---
+++ Tarea6Tests1/BLL/UsuariosTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarea6.BLL;
+using Tarea6.Entidades;
+
+namespace Tarea6Tests1.BLL
+{
+    [TestClass()]
+    public class UsuariosTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Usuarios usuarios = new Usuarios()
+            {
+                IdUsuario = 1,
+                IdEmpleado = 1,
+                Usuario = "cns",
+                Clave = "1234"
+            };
+
+            RepositorioBase<Usuarios> repositorio = new RepositorioBase<Usuarios>();
+            bool paso = false;
+            paso = repositorio.Guardar(usuarios);
+            Assert.AreEqual(true, paso);
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            RepositorioBase<Usuarios> repositorio = new RepositorioBase<Usuarios>();
+            bool paso = false;
+            Usuarios usuarios = repositorio.Buscar(1);
+            usuarios.Usuario = "aea";
+            paso = repositorio.Modificar(usuarios);
+            Assert.AreEqual(true, paso);
+        }
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            RepositorioBase<Usuarios> repositorio = new RepositorioBase<Usuarios>();
+            Usuarios usuarios = repositorio.Buscar(1);
+            Assert.IsNotNull(usuarios);
+        }
+
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            RepositorioBase<Usuarios> repositorio = new RepositorioBase<Usuarios>();
+            List<Usuarios> lista = new List<Usuarios>();
+            lista = repositorio.GetList(u => true);
+            Assert.IsNotNull(lista);
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            RepositorioBase<Usuarios> repositorio = new RepositorioBase<Usuarios>();
+            bool paso = false;
+            paso = repositorio.Eliminar(1);
+            Assert.AreEqual(true, paso);
+        }
+    }
+}
---
+++ Tarea6Tests1/BLL/VentasTests.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarea6.BLL;
+using Tarea6.Entidades;
+
+namespace Tarea6Tests1.BLL
+{
+    [TestClass()]
+    public class VentasTests
+    {
+        [TestMethod()]
+
+        public void GuardarTest()
+        {
+            Ventas ventas = new Ventas()
+            {
+
+                IdVenta = 1,
+                IdUsuario = 1,
+                IdCliente = 1,
+                IdTipoComprobante = 1,
+                FechaVenta = DateTime.Now,
+                Igv = 1,
+                SubTotal = 1,
+                CostoVenta = 1
+            };
+            RepositorioBase<Ventas> repositorio = new RepositorioBase<Ventas>();
+            bool paso = false;
+            paso = repositorio.Guardar(ventas);
+            Assert.AreEqual(true, paso);
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            RepositorioBase<Ventas> repositorio = new RepositorioBase<Ventas>();
+            bool paso = false;
+            Ventas ventas = repositorio.Buscar(1);
+            ventas.IdUsuario = 2;
+            paso = repositorio.Modificar(ventas);
+            Assert.AreEqual(true, paso);
+        }
+
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            RepositorioBase<Ventas> repositorio = new RepositorioBase<Ventas>();
+            Ventas ventas = repositorio.Buscar(1);
+            Assert.IsNotNull(ventas);
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            RepositorioBase<Ventas> repositorio = new RepositorioBase<Ventas>();
+            List<Ventas> lista = new List<Ventas>();
+            lista = repositorio.GetList(v => true);
+            Assert.IsNotNull(lista);
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            RepositorioBase<Ventas> repositorio = new RepositorioBase<Ventas>();
+            bool paso = false;
+            paso = repositorio.Eliminar(1);
+            Assert.AreEqual(true, paso);
+        }
+    }
+}
