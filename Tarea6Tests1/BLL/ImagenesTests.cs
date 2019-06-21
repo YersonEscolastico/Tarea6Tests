4b7e2cc9374fd2f780e812e33cec95bd8edc7736
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-06-21 13:46:49 +0800

    Haciendo Tests a Imagenes

---
+++ Tarea6Tests1/BLL/ImagenesTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarea6.BLL;
+using Tarea6.Entidades;
+
+namespace Tarea6Tests1.BLL
+{
+    [TestClass()]
+    public class ImagenesTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Imagenes imagenes = new Imagenes()
+            {
+                IdImagen = 1,
+                RutaImagen = "12"
+            };
+
+            RepositorioBase<Imagenes> repositorio = new RepositorioBase<Imagenes>();
+            bool paso = false;
+            paso = repositorio.Guardar(imagenes);
+            Assert.AreEqual(true, paso);
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            RepositorioBase<Imagenes> repositorio = new RepositorioBase<Imagenes>();
+            bool paso = false;
+            Imagenes imagenes = repositorio.Buscar(1);
+            imagenes.RutaImagen = "723";
+            paso = repositorio.Modificar(imagenes);
+            Assert.AreEqual(true, paso);
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            RepositorioBase<Imagenes> repositorio = new RepositorioBase<Imagenes>();
+            Imagenes imagenes = repositorio.Buscar(1);
+            Assert.IsNotNull(imagenes);
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            RepositorioBase<Imagenes> repositorio = new RepositorioBase<Imagenes>();
+            List<Imagenes> lista = new List<Imagenes>();
+            lista = repositorio.GetList(e => true);
+            Assert.IsNotNull(lista);
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            RepositorioBase<Imagenes> repositorio = new RepositorioBase<Imagenes>();
+            bool paso = false;
+            paso = repositorio.Eliminar(1);
+            Assert.AreEqual(true, paso);
+        }
+
+    }
+}
