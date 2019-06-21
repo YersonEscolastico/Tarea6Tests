f8f11f1dd5df5b5a45a31bfedf8f0b800b66e225
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-06-21 10:25:45 +0800

    Agregando entidad DetalleCompras

--- Tarea6/DAL/Contexto.cs
+++ Tarea6/DAL/Contexto.cs
@@ -17,13 +17,14 @@ namespace Tarea6.DAL
         public DbSet<Cargos> cargos { get; set; }
         public DbSet<Estados> estados { get; set; }
         public DbSet<Proveedores> proveedores { get; set; }
+        public DbSet<DetalleVentas> detalleVentas { get; set; }
         public DbSet<Ventas> ventas { get; set; }
         public DbSet<Usuarios> usuarios { get; set; }
         public DbSet<Usuarios_Has_Permisos> usuarios_has_permisos { get; set; }
         public DbSet<Imagenes> imagenes { get; set; }
         public DbSet<TiposComprobantes> tiposComprobantes { get; set; }
         public DbSet<Permisos> permisos { get; set; }
-        public DbSet<DetalleVentas> detalleVentas { get; set; }
+        public DbSet<DetalleCompras> detalleCompras { get; set; }
         public DbSet<Compras> compras { get; set; }
 
 
