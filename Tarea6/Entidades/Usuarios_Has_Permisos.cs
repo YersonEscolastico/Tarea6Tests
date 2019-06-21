b4799295444e27af57fb70449096621341d900d8
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-06-21 13:41:18 +0800

    Haciendo Tests  a Usuario_Has_Permisos

--- Tarea6/Entidades/Usuarios_Has_Permisos.cs
+++ Tarea6/Entidades/Usuarios_Has_Permisos.cs
@@ -10,7 +10,6 @@ namespace Tarea6.Entidades
     public class Usuarios_Has_Permisos
     {
         [Key]
-        public int IdUsuario_Has_Permiso { get; set; }
         public int IdUsuario { get; set; }
         public int IdPermiso { get; set; }
 
---
+++ Tarea6Tests1/BLL/Usuarios_Has_PermisosTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarea6.BLL;
+using Tarea6.Entidades;
+
+namespace Tarea6Tests1.BLL
+{
+    [TestClass()]
+    public class Usuarios_Has_PermisosTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Usuarios_Has_Permisos usuarios = new Usuarios_Has_Permisos()
+            {
+                IdUsuario = 1,
+                IdPermiso = 1
+            };
+
+            RepositorioBase<Usuarios_Has_Permisos> repositorio = new RepositorioBase<Usuarios_Has_Permisos>();
+            bool paso = false;
+            paso = repositorio.Guardar(usuarios);
+            Assert.AreEqual(true, paso);
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            RepositorioBase<Usuarios_Has_Permisos> repositorio = new RepositorioBase<Usuarios_Has_Permisos>();
+            bool paso = false;
+            Usuarios_Has_Permisos usuarios = repositorio.Buscar(1);
+            usuarios.IdPermiso = 2;
+            paso = repositorio.Modificar(usuarios);
+            Assert.AreEqual(true, paso);
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            RepositorioBase<Usuarios_Has_Permisos> repositorio = new RepositorioBase<Usuarios_Has_Permisos>();
+            Usuarios_Has_Permisos usuarios = repositorio.Buscar(1);
+            Assert.IsNotNull(usuarios);
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            RepositorioBase<Usuarios_Has_Permisos> repositorio = new RepositorioBase<Usuarios_Has_Permisos>();
+            List<Usuarios_Has_Permisos> lista = new List<Usuarios_Has_Permisos>();
+            lista = repositorio.GetList(u => true);
+            Assert.IsNotNull(lista);
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            RepositorioBase<Usuarios_Has_Permisos> repositorio = new RepositorioBase<Usuarios_Has_Permisos>();
+            bool paso = false;
+            paso = repositorio.Eliminar(1);
+            Assert.AreEqual(true, paso);
+        }
+
+    }
+}
