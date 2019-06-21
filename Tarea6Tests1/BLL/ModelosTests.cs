c0a2a9bd32de32547dd061ccdadbc472920eac41
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-06-21 15:25:53 +0800

    Haciendo Tests de Modelo y UnidadMedidas

---
+++ Tarea6Tests1/BLL/ModelosTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarea6.BLL;
+using Tarea6.Entidades;
+
+namespace Tarea6Tests1.BLL
+{
+    [TestClass]
+    public class ModelosTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Modelos modelos = new Modelos()
+            {
+                IdModelo = 0,
+                NombreModelo = "ja"
+            };
+            RepositorioBase<Modelos> repositorio = new RepositorioBase<Modelos>();
+            bool paso = false;
+            paso = repositorio.Guardar(modelos);
+            Assert.AreEqual(true, paso);
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            RepositorioBase<Modelos> repositorio = new RepositorioBase<Modelos>();
+            bool paso = false;
+            Modelos modelos = repositorio.Buscar(1);
+            modelos.NombreModelo = "sang";
+            paso = repositorio.Modificar(modelos);
+            Assert.AreEqual(true, paso);
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            RepositorioBase<Modelos> repositorio = new RepositorioBase<Modelos>();
+            Modelos modelos = repositorio.Buscar(1);
+            Assert.IsNotNull(modelos);
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            RepositorioBase<Modelos> repositorio = new RepositorioBase<Modelos>();
+            List<Modelos> lista = new List<Modelos>();
+            lista = repositorio.GetList(m => true);
+            Assert.IsNotNull(lista);
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            RepositorioBase<Modelos> repositorio = new RepositorioBase<Modelos>();
+            bool paso = false;
+            paso = repositorio.Eliminar(1);
+            Assert.AreEqual(true, paso);
+        }
+    }
+}
---
+++ Tarea6Tests1/BLL/UnidadMedidasTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarea6.BLL;
+using Tarea6.Entidades;
+
+namespace Tarea6Tests1.BLL
+{
+    [TestClass()]
+    public class UnidadMedidasTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            UnidadMedidas unidad = new UnidadMedidas()
+            {
+                IdUnidadMedida = 2,
+                NombreUnidad = "Libra"
+            };
+
+            RepositorioBase<UnidadMedidas> repositorio = new RepositorioBase<UnidadMedidas>();
+            bool paso = false;
+            paso = repositorio.Guardar(unidad);
+            Assert.AreEqual(true, paso);
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            RepositorioBase<UnidadMedidas> repositorio = new RepositorioBase<UnidadMedidas>();
+            bool paso = false;
+            UnidadMedidas unidad = repositorio.Buscar(3);
+            unidad.NombreUnidad = "Onza";
+            paso = repositorio.Modificar(unidad);
+            Assert.AreEqual(true, paso);
+        }
+
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            RepositorioBase<UnidadMedidas> repositorio = new RepositorioBase<UnidadMedidas>();
+            UnidadMedidas unidad = repositorio.Buscar(3);
+            Assert.IsNotNull(unidad);
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            RepositorioBase<UnidadMedidas> repositorio = new RepositorioBase<UnidadMedidas>();
+            List<UnidadMedidas> lista = new List<UnidadMedidas>();
+            lista = repositorio.GetList(u => true);
+            Assert.IsNotNull(lista);
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            RepositorioBase<UnidadMedidas> repositorio = new RepositorioBase<UnidadMedidas>();
+            bool paso = false;
+            paso = repositorio.Eliminar(3);
+            Assert.AreEqual(true, paso);
+        }
+    }
+}
