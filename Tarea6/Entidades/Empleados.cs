efe9c3772627efa7ec089396cc2fdfc122ad8da8
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-06-21 09:11:47 +0800

    Agregando entidad Empleados

---
+++ Tarea6/Entidades/Empleados.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea6.Entidades
+{
+    public class Empleados
+    {
+            public int IdEmpleado { get; set; }
+            public int IdEstado { get; set; }
+            public int IdCargo { get; set; }
+            public int IdPersona { get; set; }
+
+            public Empleados()
+            {
+                IdEmpleado = 0;
+                IdEstado = 0;
+                IdCargo = 0;
+                IdPersona = 0;
+            }
+        }
+}
