dd0db6b8938ea476777f1396189004fe4d551c26
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-06-21 12:35:08 +0800

    Haciendo Tests de Empleados

---
+++ Tarea6Tests1/BLL/EmpleadosTests.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarea6.BLL;
+using Tarea6.Entidades;
+
+namespace Tarea6Tests1.BLL
+{
+    [TestClass()]
+    public class EmpleadosTests
+    {
+
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Empleados empleados = new Empleados()
+            {
+
+                IdEmpleado = 1,
+                IdEstado = 1,
+                IdCargo = 1,
+                IdPersona = 1
+            };
+
+            RepositorioBase<Empleados> repositorio = new RepositorioBase<Empleados>();
+            bool paso = false;
+            paso = repositorio.Guardar(empleados);
+            Assert.AreEqual(true, paso);
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            RepositorioBase<Empleados> repositorio = new RepositorioBase<Empleados>();
+            bool paso = false;
+            Empleados empleados = repositorio.Buscar(1);
+            empleados.IdEstado = 2;
+            paso = repositorio.Modificar(empleados);
+            Assert.AreEqual(true, paso);
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            RepositorioBase<Empleados> repositorio = new RepositorioBase<Empleados>();
+            Empleados empleados = repositorio.Buscar(1);
+            Assert.IsNotNull(empleados);
+        }
+
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            RepositorioBase<Empleados> repositorio = new RepositorioBase<Empleados>();
+            List<Empleados> lista = new List<Empleados>();
+            lista = repositorio.GetList(e => true);
+            Assert.IsNotNull(lista);
+        }
+
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            RepositorioBase<Empleados> repositorio = new RepositorioBase<Empleados>();
+            bool paso = false;
+            paso = repositorio.Eliminar(1);
+            Assert.AreEqual(true, paso);
+        }
+    }
+}
